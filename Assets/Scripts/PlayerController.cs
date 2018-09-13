4b8c538f92cac4fe73e046a74d6d1ff897683b82
Von Arzu <vonarzu@gmail.com>
2018-09-13 18:30:38 -0400

    Count text
    The player is now counting and collecting, win text appears but dosent specify when a player has won

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,20 @@
 ﻿using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     public float speed;
+    public Text countText;
+    public Text winText;
 
     private Rigidbody rb;
-
+    private int count;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        
     }
     void FixedUpdate()
     {
@@ -19,21 +24,31 @@ public class PlayerController : MonoBehaviour {
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce(movement * speed);
+       
     }
 
-    public class ExampleClass : MonoBehaviour
-    {
         void OnTriggerEnter(Collider other)
         {
             {
                 if (other.gameObject.CompareTag("PickUp"))
                 {
                     other.gameObject.SetActive(false);
-                }
+                    count = count + 1;
+                    SetCountText ();
+                winText.text = "";
+        }
             }
 
         }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+        {
+            winText.text = "You Win!";
+        }
     }
-}
+    }
+
 
 
